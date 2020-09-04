f163efb982c4ce5e097fe083808e8c1e16204d53
Priya-81199 <priya81199@gmail.com>
2020-09-04 18:38:44 +0530

    File_renamed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fruitshelflifefinal/main.dart';
+import 'package:Detection_of_Artificially_Ripened_Fruit/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
